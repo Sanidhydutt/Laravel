1e36028facdad7f1e82d50d699227ba5750f39a2
SanidhyDutt <161296986+Sanidhydutt@users.noreply.github.com>
2024-08-05 13:27:05 +0530

    views_day_1
    how to add links etc

---
+++ views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1>hyy it's my first laravel project</h1>
+
+<a href="/post">2nd</a>
+
+</body>
+</html>
