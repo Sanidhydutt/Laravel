898cd1a4fa794325d6cbee5af07d3078221c1593
SanidhyDutt <161296986+Sanidhydutt@users.noreply.github.com>
2024-08-09 17:25:01 +0530

    Delete Day_1/routes_day1/Day_3 directory

--- Day_1/routes_day1/Day_3/views_day3/post.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>Post Page</h1>
-
-<a href="/">HOME</a>
-<a href="/about">about</a>
-{{-- <a href="/post">post</a> --}}
-<a href="{{route('mypost')}}">post</a>
--- Day_1/routes_day1/Day_3/views_day3/welcome.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>Welcome Page</h1>
-
-<a href="{{route('home')}}">HOME</a>
-<a href="/about">about</a>
-{{-- <a href="/post">post</a> --}}
-<a href="{{route('mypost')}}">post</a>
