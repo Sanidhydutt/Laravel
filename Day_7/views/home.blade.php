b85ee6e8ba9de332e879ac292eab9beef5a5719c
SanidhyDutt <161296986+Sanidhydutt@users.noreply.github.com>
2024-08-23 01:14:57 +0530

    Day_7

---
+++ Day_7/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Page Layout</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    @include('pages.header')
+    
+
+    <!-- Body section -->
+    <main class="body_sec">
+        <section id="Content">
+            <div class="head1">
+                <h3>My Website Home Page</h3>
+             </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus asperiores debitis beatae quaerat exercitationem commodi aliquam tempore voluptate eius, at accusantium similique voluptatem earum molestiae, nisi voluptates dicta? Voluptatum molestias cumque hic unde iusto eius inventore voluptatem, veritatis, ipsum mollitia illo vero corrupti amet sequi quibusdam impedit rerum! Eveniet, earum!</p>
+        </section>
+    </main>
+
+   @include('pages.footer')
+</body>
+
+</html>
