5f7dd88bfe317baa34fd6b896815d931dd8e90c2
SanidhyDutt <161296986+Sanidhydutt@users.noreply.github.com>
2024-10-14 00:12:44 +0530

    Add files via upload

---
+++ Day_13/app/Http/Controllers/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function showHome() 
+    {
+        return view('welcome');
+    //    return "<h1>Welcome To Controller Lecture</h1>"; 
+    }
+
+    public function showUser(string $id) 
+    {
+        
+        return view('user',compact('id'));
+        // return view('user',['id' => $id]);
+    }
+}
