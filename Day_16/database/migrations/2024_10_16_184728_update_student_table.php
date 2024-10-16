5752180f3aa20cbf97610d3f7f8e59a7c7c48125
SanidhyDutt <161296986+Sanidhydutt@users.noreply.github.com>
2024-10-17 00:30:43 +0530

    Add files via upload

---
+++ Day_16/database/migrations/2024_10_16_184728_update_student_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('student', function (Blueprint $table) {
+            //$table->renameColumn('city','cities');
+            $table->dropColumn('percentage');
+            // if city not added by default added no city
+            $table->string('city', 100)->default('No City')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('student', function (Blueprint $table) {
+            //
+        });
+    }
+};
