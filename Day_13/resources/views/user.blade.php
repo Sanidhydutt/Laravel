98dae599758710510316357954a342ac685b266e
SanidhyDutt <161296986+Sanidhydutt@users.noreply.github.com>
2024-10-18 00:29:34 +0530

    Add files via upload

---
+++ Day_13/resources/views/user.blade.php
@@ -0,0 +1,4 @@
+<h1>User Page</h1>
+{{ $id }}
+<a href="{{ route('home') }}">Home Page</a> <br>
+
---
+++ Day_13/routes/web.php
@@ -0,0 +1,21 @@
+<?php 
+
+use Illuminate\Support\Facades\Route;
+use  App\Http\Controllers\PageController;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('welcome');
+
+
+
+Route::get('/',[PageController::class,'showHome'])->name('home');
+
+Route::get('/user/{id}',[PageController::class,'showUser'])->name('user');
+
+Route::fallback(function () {
+    return "<h1>404 page not found</h1>";
+});
+
+
+?>
