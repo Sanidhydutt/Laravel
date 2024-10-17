e29148656db3894c07e836bbc4a4f8fa6d0b0d62
SanidhyDutt <161296986+Sanidhydutt@users.noreply.github.com>
2024-10-17 18:43:21 +0530

    web.php

--- Day_13/routes/web.php
+++ Day_13/routes/web.php
@@ -3,15 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use  App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('welcome');
 
 
 
 Route::get('/',[PageController::class,'showHome'])->name('home');
 
-Route::get('/user/{id}',[PageController::class,'showUser'])->name('user');
+Route::get('/user',[PageController::class,'showUser'])->name('user');
+
+Route::get('/user/{id}',[PageController::class,'showUserid'])->name('id');
 
 Route::fallback(function () {
     return "<h1>404 page not found</h1>";
